773ba6e6e85eb4f8dfdd8eb5a9e6f5712908bf03
TuongVy06-20PMC <0306201306@caothang.edu.vn>
2022-12-22 19:42:09 +0700

    update

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/Screen/BatDauChoi.dart';
+import 'package:flutter_application_2/Screen/Loading.dart';
 import 'package:flutter_application_2/Screen/TimTran.dart';
 import 'package:flutter_application_2/Screen/ThongKe.dart';
 import 'package:flutter_application_2/Screen/TrangChu.dart';
@@ -8,7 +11,9 @@ import 'package:flutter_application_2/Screen/XepHang.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'Screen/Screen1.dart';
 
-void main() {
+Future<void> main(List<String> agrs) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -19,68 +24,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        initialRoute:
+            FirebaseAuth.instance.currentUser == null ? 'Loading' : 'TrangChu',
+        routes: {
+          'TrangChu': (context) => TrangChuTabBarGoogle(),
+          'Loading': (context) => Loading()
+        });
   }
 }
-
-//123
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/bgg.jpg'), fit: BoxFit.cover)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-                  margin: EdgeInsets.only(bottom: 80, top: 150),
-                  child: Image.asset(
-                    'assets/logo.png',
-                    fit: BoxFit.cover,
-                    width: 200,
-                  ),
-                ),
-          Padding(
-            padding: EdgeInsets.only(top: 350),
-            child: TextButton(
-              child: const Text(
-                'Bắt đầu.......',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 35,
-                  fontFamily: 'FSAriston',
-                  color: Colors.black,
-                ),
-              ),
-              onPressed: () {
-                Navigator.popUntil(context, (route) => route.isFirst);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const Screen1()),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    )));
-  }
-}// thay doi
